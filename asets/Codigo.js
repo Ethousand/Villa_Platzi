e4ecc3a325c3ad8e8e054dcc7b70e4c538318857
Emil Colina <cybre333@gmail.com>
2022-02-15 01:53:41 -0400

    tarea realizada, agregada posicion aleatorias y set para mover un personaje tarea realizada, agregada posicion aleatorias y set para mover un personaje
    tarea realizada, agregada posicion aleatorias y set para mover un personaje
    
    tarea realizada, agregada posicion aleatorias y set para mover un personaje
    tarea realizada, agregada posicion aleatorias y set para mover un personaje
    
    tarea realizada, agregada posicion aleatorias y set para mover un personaje

--- asets/Codigo.js
+++ asets/Codigo.js
@@ -1,23 +1,196 @@
-//l = document.getElementById("campo");
-boton = document.getElementById("botoncito");
-//alert("js en prueba");
+var vp = document.getElementById("campo");
+papel = vp.getContext("2d");
 
+document.addEventListener("keydown", mover_Dim);
 
-//document.write("<br> esta es una prueba " + " el valor es " + z);
-var z = boton.addEventListener("click", random);
+var n, mov = 10; // variable contadora, para los ciclos for
 
-
-
-
-function random(min, max)
+tecla = 
 {
-    get_min = document.getElementById("caja_min");
-    get_max = document.getElementById("caja_max");
-    //console.log("este es el minimo " + get_min.value + " este es el maximo" + get_max.value);
-    maximo = parseInt(get_max.value);
-    minimo = parseInt(get_min.value);
-    var result = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
-    document.write(result);
-    //console.log(result);
-    return result;    
+    Left: 37,    Up: 38,
+    Right: 39,   Down: 40
+}
+
+// objetos literles para las caracteristicas de cada animal y sus posiciones dentro del canvas
+
+var vaca =
+{
+    url: "asets/vaca.png",
+    loaded: false,
+    cantidad: randomizer(1, 20)
+}
+
+var Dim = {
+    url: "asets/Dim.png",
+    loaded: false,
+    x: 200,
+    y:200
+}
+
+var pollo = 
+{
+    url:"asets/pollo.png",
+    loaded: false,
+    cantidad: randomizer(3, 25)
+}
+
+var cerdo =
+{
+    url: "asets/cerdo.png",
+    loaded:false,
+    cantidad: randomizer(2, 15)
+}
+
+var fondo =
+{
+    url: "asets/tile.png",
+    loaded: false
+}
+ 
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.position_x = [];
+cerdo.position_y = [];
+cerdo.imagen.addEventListener("load", cargar_cerdo);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.position_x = [];
+pollo.position_y = [];
+pollo.imagen.addEventListener("load", cargar_pollo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.position_x = [];
+vaca.position_y = [];
+vaca.imagen.addEventListener("load", cargar_vaca);
+
+Dim.imagen = new Image();
+Dim.imagen.src = Dim.url;
+Dim.imagen.addEventListener("load", cargar_Dim);
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargar_fondo);
+
+
+
+function cargar_vaca()
+{
+   
+    vaca.loaded = true;
+    for (n = 0; n < vaca.cantidad; n ++)
+    {
+        vaca.position_y[n] = randomizer(0, 8)*60;
+        vaca.position_x[n] = randomizer(0, 8)*60;
+    }   
+    dibujar();    
+}
+
+function cargar_pollo()
+{
+    for (n = 0; n < pollo.cantidad; n ++)
+    {
+        pollo.position_y[n] = randomizer(0, 12)*35;
+        pollo.position_x[n] = randomizer(0, 12)*35;
+    } 
+    pollo.loaded = true;
+    dibujar();
+}
+
+function cargar_cerdo()
+{
+    for (n = 0; n < vaca.cantidad; n ++)
+    {
+        cerdo.position_y[n] = randomizer(0, 10)*42;
+        cerdo.position_x[n] = randomizer(0, 10)*42;
+    } 
+    cerdo.loaded = true;
+    dibujar();    
+}
+
+function cargar_fondo()
+{
+    fondo.loaded = true;
+    dibujar();    
+}
+
+function cargar_Dim() 
+{
+    Dim.loaded = true;
+    dibujar();
+}
+
+function mover_Dim(event)
+{
+    if (event.keyCode == tecla.Up)
+    {
+        Dim.y = Dim.y - mov;
+        console.log(Dim.y);
+        dibujar();
+    }
+
+    if (event.keyCode == tecla.Down)
+    {
+        Dim.y = Dim.y + mov;
+        dibujar();
+    }
+
+    if (event.keyCode == tecla.Left)
+    {
+        Dim.x = Dim.x - mov;
+        dibujar();
+    }
+
+    if (event.keyCode == tecla.Right)
+    {
+        Dim.x = Dim.x + mov;
+        dibujar();
+    }
+}
+
+function dibujar()
+{
+    if(fondo.loaded)
+    {
+        papel.drawImage(fondo.imagen, 0, 0);
+    }
+
+    if (cerdo.loaded)
+    {
+        for(n = 0; n < cerdo.cantidad; n++)
+        {
+            papel.drawImage(cerdo.imagen, cerdo.position_x[n], cerdo.position_y[n]);
+        }
+    }
+
+    if (pollo.loaded)
+    {
+        for(n = 0; n < pollo.cantidad; n++)
+        {
+            papel.drawImage(pollo.imagen, pollo.position_x[n], pollo.position_y[n]);
+        }
+    }
+
+    if (Dim.loaded)
+    {
+        papel.drawImage(Dim.imagen, Dim.x, Dim.y);
+        console.log(Dim.x);
+            
+    }
+
+    if (vaca.loaded)
+    {
+        for(n = 0; n < vaca.cantidad; n++)
+        {
+            papel.drawImage(vaca.imagen, vaca.position_x[n], vaca.position_y[n]);
+        }
+        
+    }
+}
+
+function randomizer(min, max)
+{
+    var resultado = Math.floor(Math.random()* (max - min)) + min;
+    return resultado;
 }
